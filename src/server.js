ab47ebec232b3de2c89705b507f605b1b42f2abc
Pedro Henrique <peixotopedro815@hotmail.com>
2026-08-21 09:29:06 -0300

    chore: initialize backend structure

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "OK",
+        service: "Garage System",
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Garage System is running on port ${PORT}`);
+});
