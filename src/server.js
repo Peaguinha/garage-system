d98b5fa08f2b5c86065a5b71ac79c56118749143
Pedro Henrique <peixotopedro815@hotmail.com>
2026-08-28 10:49:19 -0300

    fix: organize middleware and JWT routes in server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
+
+const authMiddleware = require("./middlewares/authMiddleware");
+const roleMiddleware = require("./middlewares/roleMiddleware");
 
 const app = express();
 
@@ -7,15 +10,35 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Health Check
 app.get("/health", (req, res) => {
     res.json({
         status: "OK",
         service: "Garage System",
-    })
+    });
 });
+
+// Rota protegida - autenticação
+app.get("/protected", authMiddleware, (req, res) => {
+    res.status(200).json({
+        message: "Acesso autorizado",
+        user: req.user
+    });
+});
+
+// Rota protegida - autenticação + autorização
+app.get(
+    "/admin",
+    authMiddleware,
+    roleMiddleware("ADMIN"),
+    (req, res) => {
+        res.status(200).json({
+            message: "Acesso autorizado",
+            user: req.user
+        });
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`Garage System is running on port ${PORT}`);
 });
-
-start();
